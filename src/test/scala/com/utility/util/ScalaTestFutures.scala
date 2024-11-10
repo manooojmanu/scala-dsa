afbdf2090fb8532c0a5bf5593d1f3b9551ccabfe
Manoj Kumar <manoj.mallappa@man-es.com>
2024-11-10 21:01:23 +0530

    removed test files

--- src/test/scala/com/utility/util/ScalaTestFutures.scala
+++
@@ -1,8 +0,0 @@
-package com.utility.util
-
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{Seconds, Span}
-
-trait ScalaTestFutures extends ScalaFutures {
-  override implicit val patienceConfig: PatienceConfig = super.patienceConfig.copy(timeout = scaled(Span(150, Seconds)))
-}
